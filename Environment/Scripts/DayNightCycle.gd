63a565309bd580999f24950010ba6be059cb87ac
Mesiow <34993144+Mesiow@users.noreply.github.com>
2019-06-19 15:34:43 -0400

    added simple firepit

--- Environment/Scripts/DayNightCycle.gd
+++ Environment/Scripts/DayNightCycle.gd
@@ -1,20 +1,37 @@
 extends Node2D
 
 const dayMaxTime = 1000 #1 minute
-const nightMaxTime = 1 #1 minute
+const nightMaxTime = 1000 #1 minute
 
 onready var cycle=$CanvasLayer/Cycle
+onready var firepitLight =get_parent().get_node("Firepit").get_node("Light")
 
-var played = false
+var night = true
+var day = false
 
 func _ready():
 	set_process(true)
+	
+	cycle.play("NightTransition")
+	night = true
 	pass
 	
 func _process(delta):
-	if OS.get_ticks_msec() >= dayMaxTime:
-		if !played:
-			cycle.play("NightTransition")
-			played = true
+	if cycle.is_playing() and night:
+		firepitLight.energy += 0.0001
+	elif cycle.is_playing() and day:
+		firepitLight.energy -= 0.001
 	pass
 	
+func startDay():
+	cycle.play_backwards("NightTransition")
+	day = true
+	night = false
+	pass
+
+func _on_Cycle_animation_finished(anim_name):
+	if anim_name == "NightTransition":
+		yield(get_tree().create_timer(3), "timeout") #wait 1 minute till day
+		startDay()
+	pass
+
